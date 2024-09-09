1e89e850d593772e92830012e24f1a3e976f04e8
windows <supercool4444@protonmail.com>
2024-09-08 22:39:16 -0600

    windows

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import type { AppProps } from 'next/app';
+
+import "../styles/globals.css";
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ConfigProvider >
+    <Component {...pageProps} />
+  </ConfigProvider>
+);
+
+export default App;
